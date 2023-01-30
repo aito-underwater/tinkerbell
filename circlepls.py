7da3e3629cd46d3d3c5b741b6e696277d345c518
Emirhan <Emirhan@gmail.com>
2023-01-30 20:31:22 +0300

    Yeni kare tanıma kodları_8

--- circlepls.py
+++ circlepls.py
@@ -11,7 +11,7 @@ while True:
     upper_red = np.array([10, 255, 255])
     mask = cv2.inRange(hsv, lower_red, upper_red)
 
-    contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     for contour in contours:
         if cv2.contourArea(contour) < 500:
             continue
